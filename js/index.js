53b5d50547c757bca248a8236da09755c5f8c1d7
Juan Granados <juangramd@gmail.com>
2022-05-15 18:01:12 -0500

    Animación botón

--- js/index.js
+++ js/index.js
@@ -15,3 +15,12 @@ $(window).on("scroll", function() {
           $('nav').removeClass('black');
     }
 })
+
+const btn = document.querySelector('.btn');
+		btn.onmousemove = function(e){
+			const x = e.pageX - btn.offsetLeft;
+			const y = e.pageY - btn.offsetTop;
+
+			btn.style.setProperty('--x', x + 'px');
+			btn.style.setProperty('--y', y + 'px');
+		}
